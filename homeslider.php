264d9afe1609a65c4958d9344d9fccb8d99aab1f
Shrikanth Basa <shrikanthbasa35@gmail.com>
2019-07-28 20:04:47 +0530

    Updated slider and index.php

--- homeslider.php
+++ homeslider.php
@@ -1,182 +1,121 @@
+
 <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Swiper</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 
-  <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="./slick/slick.css">
-  <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css">
-  <style type="text/css">
+  <!-- Link Swiper's CSS -->
+  <link rel="stylesheet" href="assets/css/swiper.min.css">
 
-    h3{
-      color: #bf9b30;
-    }
+  <!-- Demo styles -->
+  <style>
+  .swiper-container {
+   width: 97%;
+   padding-top: 50px;
 
-    .slider {
-        width: 100%;
-        height: 50%;
-    }
-
-    .slick-dots li button:before {
-        color: red;
-      }
-
-      .slick-dots li button {
-        color: green;
-      }
-
-
-    .slick-prev:before,
-    .slick-next:before {
-      color: black;
-    }
-
-    .slick-slide {
-      margin: 0px 20px;
-    }
-
-    .slick-slide img {
-      width: 100%;
-    }
-
-    .slick-slide {
-      transition: all ease-in-out .3s;
-      opacity: .2;
-    }
-
-    .slick-active {
-      opacity: .5;
-    }
-
-    .slick-current {
-      opacity: 1;
-    }
-
-    .slick-current img{
-      opacity: 1;
-    }
-
-    .slick-current h3{
-      display: block;
-      position: relative;
-      margin: auto;
-    }
-
-    .regular{
-      position: relative;
-    }
-
-    @media screen and (max-width: 768px) {
-      .slick-active {
-        opacity: 1;
-      }
-      .slick-slide {
-        transition: all ease-in-out .3s;
-        opacity: 1;
-      }
-    }
-
-    @media screen and (max-width: 480px) {
-      .slick-active {
-        opacity: 1;
-      }
-      .slick-slide {
-        transition: all ease-in-out .3s;
-        opacity: .2;
-      }
-      .slick-current {
-        opacity: 1;
-      }
-    }
+ }
+ .swiper-slide {
+   background-position: center;
+   background-size: cover;
+   width: 300px;
+   height: 300px;
+ }
+ .swiper-slide img{
+   width:100%;
+   border-radius: 1%;
+ }
 
   </style>
+</head>
+<body>
+  <!-- Swiper -->
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <a href="single.php">
+          <img src="withouttext/threading1.jpg" alt="Salon At Home Threading At Home" /> </a>
+          <h3>Threading<h3>
+      </div>
+      <div  class="swiper-slide">
+        <a href="waxing.php">
+            <img src="withouttext/waxing1.jpg" alt="Salon At Home Waxing At Home" /> </a>
+            <h3>Waxing<h3>
+      </div>
+      <div  class="swiper-slide">
+        <a href="cleanup.php">
+        <img src="withouttext/cleanup1.jpg" alt="Salon At Home Cleanup At Home" /> </a>
+        <h3>Cleanup<h3>
+      </div>
+      <div  class="swiper-slide">
+        <a href="facial.php">
+          <img src="withouttext/facial1.jpg" alt="Salon At Home Facial At Home" /> </a>
+          <h3>Facial<h3>
+      </div>
+      <div  class="swiper-slide">
+        <a href="bleaching.php">
+        <img src="withouttext/bleach1.jpg" alt="Salon At Home Bleach At Home" /> </a>
+        <h3>Bleaching<h3>
+      </div>
+      <div  class="swiper-slide">
+        <a href="pedicure.php">
+          <img src="withouttext/manicure1.jpg" alt="Salon At Home Pedicure/Manicure At Home" /> </a>
+          <h3>Manicure/Pedicure<h3>
+      </div>
+      <div  class="swiper-slide">
+          <a href="hairstyling.php">
+        <img src="withouttext/hairstyle1.jpg" alt="Salon At Home Hairstyling At Home" /> </a>
+        <h3>Haistyling<h3>
+      </div>
+      <div  class="swiper-slide">
+        <a href="partyready.php">
+          <img src="withouttext/partyready1.jpg" alt="Salon At Home Party Ready At Home" /> </a>
+          <h3>Party Ready<h3>
+      </div>
+      <div class="swiper-slide">
+        <a href="dressup.php">
+          <img src="withouttext/makeup1.jpg" alt="Salon At Home Dressup At Home" /> </a>
+          <h3>Dressup<h3>
+      </div>
+    </div>
+    <!-- Add Pagination -->
 
-
-
-  <div class="regular slider">
-        <div>
-          <a href="single.php">
-            <img src="withouttext/threading1.jpg" alt="Salon At Home Threading At Home" /> </a>
-            <h3>Threading<h3>
-        </div>
-        <div>
-          <a href="waxing.php">
-              <img src="withouttext/waxing1.jpg" alt="Salon At Home Waxing At Home" /> </a>
-              <h3>Waxing<h3>
-        </div>
-        <div>
-          <a href="cleanup.php">
-          <img src="withouttext/cleanup1.jpg" alt="Salon At Home Cleanup At Home" /> </a>
-          <h3>Cleanup<h3>
-        </div>
-        <div>
-          <a href="facial.php">
-            <img src="withouttext/facial1.jpg" alt="Salon At Home Facial At Home" /> </a>
-            <h3>Facial<h3>
-        </div>
-        <div>
-          <a href="bleaching.php">
-          <img src="withouttext/bleach1.jpg" alt="Salon At Home Bleach At Home" /> </a>
-          <h3>Bleaching<h3>
-        </div>
-        <div>
-          <a href="pedicure.php">
-            <img src="withouttext/manicure1.jpg" alt="Salon At Home Pedicure/Manicure At Home" /> </a>
-            <h3>Manicure/Pedicure<h3>
-        </div>
-        <div>
-            <a href="hairstyling.php">
-          <img src="withouttext/hairstyle1.jpg" alt="Salon At Home Hairstyling At Home" /> </a>
-          <h3>Haistyling<h3>
-        </div>
-        <div>
-          <a href="partyready.php">
-            <img src="withouttext/partyready1.jpg" alt="Salon At Home Party Ready At Home" /> </a>
-            <h3>Party Ready<h3>
-        </div>
-        <div class="items">
-          <a href="dressup.php">
-            <img src="withouttext/makeup1.jpg" alt="Salon At Home Dressup At Home" /> </a>
-            <h3>Dressup<h3>
-        </div>
   </div>
 
+  <!-- Swiper JS -->
+  <script src="assets/js/swiper.min.js"></script>
 
-  <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-  <script src="./slick/slick.min.js" type="text/javascript" charset="utf-8"></script>
-  <script type="text/javascript">
-    $('.regular').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    swipeToSlide: true,
-    pauseOnHover:false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots:true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: false,
-          centerPadding: '40px',
-          slidesToShow: 1,
-          pauseOnHover:false,
-            swipeToSlide: true,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          swipeToSlide: false,
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1,
-          pauseOnHover:false,
-          dots:true,
-
-        }
-      }
-    ]
-  });
-</script>
+  <!-- Initialize Swiper -->
+  <script>
+  var swiper = new Swiper('.swiper-container', {
+   slidesPerView: 4,
+   spaceBetween: 50,
+   autoplay: true,
+   loop: true,
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+   },
+   breakpoints: {
+     1024: {
+       slidesPerView: 4,
+       spaceBetween: 40,
+     },
+     768: {
+       slidesPerView: 3,
+       spaceBetween: 30,
+     },
+     640: {
+       slidesPerView: 2,
+       spaceBetween: 20,
+     },
+     320: {
+       slidesPerView: 1,
+       spaceBetween: 10,
+     }
+   }
+ });
+  </script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -11,8 +11,24 @@
 				<h1 style="margin-top: 20%;"><a href="main.php" style="font-family: 'Cinzel', serif;"><button class="btn btn-success">Shop Now</button></a></h1>
 				<h5 style="padding-top: 0px; font-size: 30px;font-family: 'Amiri', serif;">I’ve never seen a smiling face that was not beautiful...</h5>
    		</div>
-
-
-
+			
+			<script src="js/jquery.min.js"></script>
+			<!-- jQuery Easing -->
+			<script src="js/jquery.easing.1.3.js"></script>
+			<!-- Bootstrap -->
+			<script src="js/bootstrap.min.js"></script>
+			<!-- Waypoints -->
+			<script src="js/jquery.waypoints.min.js"></script>
+			<!-- Carousel -->
+			<script src="js/owl.carousel.min.js"></script>
+			<!-- countTo -->
+			<script src="js/jquery.countTo.js"></script>
+			<!-- Flexslider -->
+			<script src="js/jquery.flexslider-min.js"></script>
+			<!-- Main -->
+			<script src="js/main.js"></script>
+			<!--Slider Menu -->
+			<script type="text/javascript" src="jss/owl.carousel.min.js"></script>
+			<script type="text/javascript" src="jss/script.js"></script>
 </body>
 </html>
